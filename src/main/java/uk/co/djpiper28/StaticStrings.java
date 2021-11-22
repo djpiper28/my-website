e833d1cd927f2ef61a0a077bf9dcc78c1798c974
Danny Piper <djpiper28@gmail.com>
2021-11-22 00:29:28 +0000

    added some more text you know

--- src/main/java/uk/co/djpiper28/StaticStrings.java
+++ src/main/java/uk/co/djpiper28/StaticStrings.java
@@ -215,22 +215,37 @@ public class StaticStrings {
      * the HTML for the about me blurb under my mugshot
      */
     public static final String ABOUT_ME_HTML = "<p class=\"middle-text\">\n" +
+            "<h4>Intro</h4>\n" +
             "Hello! I am Danny Piper (they/them) and, I am a student at Royal Holloway studying an embedded master in Computer Science\n" +
             "(Information Security). I am currently in my " + YEAR_NUMBER + " and enjoying it.<br>\n" +
             "I am an avid programmer and mainly program in Python, Java and, C. In-fact, this website is built with Java and Jetty,\n" +
             "Although I do prefer C for programming these days.<br>\n" +
-            "I am passionate linux user and FOSS fan; I have been contributing in the MTG community lately to produce better " +
+            "<h4>FOSS</h4>\n"+
+            "I am passionate linux user and FOSS fan." +
+            "<h5>Maze Programs</h5>\n" +
+            "<h6>Maze Generators/h6>\n" +
+            "<a href=\"https://github.com/djpiper28/Maze-Generator\">Java Maze Generator</a> - my first attempt at generating mazes, this was quite bad but fun to make.\n" +
+            "<a href=\"https://github.com/djpiper28/ANSI-C-Maze-Generator\">ANSI C Maze Generator</a> - my latest and greatest maze generator. This one is really fast and makes large mazes quickly.\n" +
+            "\n" +
+            "<h6>Maze Solver</g6>\n" +
+            "<a href=\"https://github.com/djpiper28/MazeSolver-javafx\">Java Maze Solver</a> - my first, decent attempt at solving mazes. It has a few cool methods to do so and works fairly well.\n" +
+            "<a href=\"https://github.com/djpiper28/OpenCLTest\">OpenCL Maze Solver<a> - this is my first opencl program and is a fairly fussy but fast maze solver. Some input really upsets it though." +
+            "<h5>MTG Tournament Programs</h5>\n" +
+            "I have been contributing in the MTG community lately to produce better " +
             "software for running tournaments (see <a href=\"https://gitlab.com/monarch3\">monarch3's</a> gitlab page " +
             "and <a href=\"https://github.com/djpiper28/CockatriceTournamentBot\">my cockatrice bot's</a> (I call this tricebot) github page to" +
             "see some of these softwares). I have been working with monarch3 for a while now (since around late February 2021) " +
             "and have been  actively working on <a href=\"https://gitlab.com/monarch3/SquireBot\">Squire Bot</a>, which " +
             "just so happens to live on this server along with tricebot.<br>\n" +
+            "<h4>Self Hosting</h4>\n"+
             "Lately I have been getting into system administration and hosting; to help me with this I brought a second hand " +
             "server off of eBay (a Dell PowerEdge R520 with 32 GiB for those who are interested). Unfortunately for me I have " +
             "to sleep in the same room as my server. But it is worth it because it means that I can continue learning about " +
             "hosting and system administration.<br>\n" +
+            "<h4>Other Hobbies</h4>\n" +
             "Apart from computers and linux, I am also a fan of miniatures (painting and building Warhammer 40k usually), " +
             "listening to power/industrial metal and drinking coffee.<br><br>\n" +
+            "<h4>Contact Me</h4>\n" +
             "You can contact me via email (devdannypiper@gmail.com)<br>\n" +
             "Or via discord (Danny P.#6969)<br>" +
             "But please don't spam me." +
