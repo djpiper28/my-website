360068d300fff99a4620a7c80c50b5cc3f0dd159
Kai Piper <kai9987kai@gmail.com>
2023-05-31 15:52:38 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,3 +9,21 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+window.onscroll = function() {
+  showScrollToTopButton();
+};
+
+function showScrollToTopButton() {
+  var scrollToTopButton = document.getElementById("scrollToTopButton");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
