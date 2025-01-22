74f054801048b4dc513968c5009bab00e6fd2f0c
Danny Piper <djpiper28@gmail.com>
2025-01-22 23:51:30 +0000

    feat: use NEXTJS

--- src/Repos.jsx
+++
@@ -1,9 +0,0 @@
-// In the form repo/name
-const Repos = [
-  "MonarchDevelopment/mtg-search-engine",
-  "MonarchDevelopment/SquireDesktop",
-  "MonarchDevelopment/SquireCore",
-  "rhul-compsoc/tito-integration-hackaway",
-  "arcio-uk/Attendance-Backend",
-];
-export default Repos;
