ac6c381ee9ade952669d4c631a5678384d58c508
Danny Piper <djpiper28@gmail.com>
2025-02-21 16:07:03 +0000

    feat: added some styles

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -1,19 +1,27 @@
 import { githubUrl } from "@/consts";
 import { Link } from "../typrography/Link";
+import { P } from "../typrography/P";
 
 export function Footer() {
   return (
     <footer className="flex flex-col md:px-10 p-2 bg-slate-300">
       <p>
-        Danny Piper 2025, Unlicense,{" "}
+        (C) Danny Piper 2025, Unlicense,{" "}
         <Link href={githubUrl + "/my-website"}>Source Code</Link>
       </p>
-      <p>
-        My <Link href={githubUrl}>Github</Link>
-      </p>
-      <p>
-        <Link href={"/contact-me"}>Contact me</Link>
-      </p>
+      <div className="flex flex-row flex-wrap gap-2">
+        <p>
+          <Link href={githubUrl}>My Github</Link>
+        </p>
+        <Sep />
+        <p>
+          <Link href={"/contact-me"}>Contact me</Link>
+        </p>
+      </div>
     </footer>
   );
+}
+
+function Sep() {
+  return <P className="aria-hidden">{" | "}</P>;
 }
--- src/components/typrography/P.tsx
+++ src/components/typrography/P.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from "react";
 
 interface Props {
+  className?: string;
   children: string | ReactNode;
 }
 
 export function P(props: Readonly<Props>) {
-  return <p className="text-lg">{props.children}</p>;
+  return <p className="text-lg ${props.className}">{props.children}</p>;
 }
