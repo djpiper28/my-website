c693c67d95494b8501326ec784274d4a01481ab1
Danny Piper <djpiper28@gmail.com>
2025-07-26 22:56:44 +0100

    fix: dates lol JS is shit

--- src/app/blog/2025/05/moving_away_from_linkedin/meta.tsx
+++ src/app/blog/2025/05/moving_away_from_linkedin/meta.tsx
@@ -3,5 +3,5 @@ import { BlogMeta } from "@/app/blog/meta";
 export const meta: BlogMeta = {
   title: "Moving Away From LinkedIn",
   description: "A small rant about LinkedIn and other social media",
-  release: new Date(2025, 5, 19),
+  release: new Date(2025, 4, 19),
 }
--- src/app/blog/2025/06/age_checking_on_websites/meta.tsx
+++ src/app/blog/2025/06/age_checking_on_websites/meta.tsx
@@ -4,5 +4,5 @@ export const meta: BlogMeta = {
   title: "Age Checking on Websites",
   description:
     "I discuss privacy concerns, mitigations, current technology, and if it should even be done.",
-  release: new Date(2025, 6, 17),
+  release: new Date(2025, 5, 17),
 };
