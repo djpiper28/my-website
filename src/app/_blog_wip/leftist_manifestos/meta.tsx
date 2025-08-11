ad39fec7607caca4d2252600c1eca095ca07bf3b
Danny Piper <danny.piper@motorolasolutions.com>
2025-08-11 15:54:14 +0100

    removed stuff

--- src/app/_blog_wip/leftist_manifestos/meta.tsx
+++
@@ -1,8 +0,0 @@
-import { BlogMeta } from "@/app/blog/meta";
-
-export const meta: BlogMeta = {
-  title: "A Unified Leftist Manifesto",
-  description:
-    "A left wing Manifesto that looks to change how things work, and fix what is broken.",
-  release: new Date(2025, 7, 1),
-};
--- src/app/blog/2025/08/online_unsafety_act/meta.tsx
+++
@@ -1,8 +0,0 @@
-import { BlogMeta } from "@/app/blog/meta";
-
-export const meta: BlogMeta = {
-  title: "Online Unsafety Act & The War On Encryption",
-  description:
-    "A talk about the UK trying to make the internet more unsafe and brand those who oppose it as being on the side of Jimmy Saville. In other news please use your credit card to login to your favourite Russian porn site, and upload your passport to use Twitter.",
-  release: new Date(2025, 7, 2),
-};
