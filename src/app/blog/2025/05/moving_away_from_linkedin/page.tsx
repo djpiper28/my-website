6aaae7d3ad66d11e3cc92531370f7a8eec840f9b
Danny Piper <danny.piper@motorolasolutions.com>
2025-09-26 11:14:41 +0100

    fix: many typos

--- src/app/blog/2025/05/moving_away_from_linkedin/page.tsx
+++ src/app/blog/2025/05/moving_away_from_linkedin/page.tsx
@@ -8,16 +8,16 @@ export default function Page() {
       <H1>Moving Away From LinkedIn</H1>
       <H2>It Feels Like Facebook</H2>
       <P>
-        LinkedIn is full of virtual signalling, low quality posts, and
+        LinkedIn is full of virtue signaling, low quality posts, and
         connection requests from what feels like bots. Similar to Facebook the
-        platform feels setup to faciliate spam arriving in your inbox. Most
+        platform feels setup to facilitate spam arriving in your inbox. Most
         content on the platform is either as simple as a Google search AI
         result, or just irrelevant stuff about &quot;digital nomads&quot;, or
         some other trend that should be relegated to TikTok. Then you get the
         spam connection requests, either by recruiters who have no intention to
         recruit you, or by someone trying to sell you another course. The
         website does not feel like it is for professionals to talk about their
-        profession, instead it feels like a coorporate circle jerk of
+        profession, instead it feels like a corporate circle jerk of
         nothingness.
       </P>
       <H2>Surface Level Posts</H2>
@@ -26,7 +26,7 @@ export default function Page() {
         level. You will find a lot of &quot;We migrated database at our
         startup&quot; type posts, which often fail to discuss anything of value.
         No cost based evaluations, no technical reasons, no graphs, etc... Often
-        times these posts read more like a Stack Overflow page, not a tehnical
+        times these posts read more like a Stack Overflow page, not a technical
         blog.
       </P>
       <H2>Mental Health</H2>
