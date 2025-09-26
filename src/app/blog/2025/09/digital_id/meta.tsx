bca447fc9aef1467907ab37b47bf54efc07c309c
Danny Piper <danny.piper@motorolasolutions.com>
2025-09-26 12:19:02 +0100

    fix: whoops deleted the rename

---
+++ src/app/blog/2025/09/digital_id/meta.tsx
@@ -0,0 +1,8 @@
+import { BlogMeta } from "@/app/blog/meta";
+
+export const meta: BlogMeta = {
+  title: "Digital ID In The UK",
+  description:
+    'Tony Blair\'s failed Digital ID is coming back. Is it a "cynical ploy" to gather more data on citizens?',
+  release: new Date(2025, 9 - 1, 26),
+};
