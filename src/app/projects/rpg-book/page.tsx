5512d0c6ebd4d09709756bbee7fbc80b5a7cc95c
Danny Piper <djpiper28@gmail.com>
2026-02-21 13:27:41 +0000

    feat: started work on RPG Book project page

---
+++ src/app/projects/rpg-book/page.tsx
@@ -0,0 +1,16 @@
+import { H1 } from "@/components/typrography/H1";
+import { P } from "@/components/typrography/P";
+
+export default function Page() {
+  return (
+    <>
+      <H1>RPG Book - An All-In-One Tool for GMs</H1>
+      <P>
+        RPG Book aims to solve a problem that I have been suffering from as a GM
+        (game master) - keeping my notes organised. I am designing the software
+        to replace the scattered notepads and maps to make my life as a GM
+        easier.
+      </P>
+    </>
+  );
+}
